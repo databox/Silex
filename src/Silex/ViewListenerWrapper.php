5d1c3d7d3fcbef13032831a702baa54bfbbbfcb7
Bojan Kovacec <bojan.kovacec@gmail.com>
2023-02-01 14:11:17 +0100

    RequestEvent

--- src/Silex/ViewListenerWrapper.php
+++ src/Silex/ViewListenerWrapper.php
@@ -13,6 +13,7 @@ namespace Silex;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 
 /**
  * Wraps view listeners.
@@ -36,25 +37,23 @@ class ViewListenerWrapper
         $this->callback = $callback;
     }
 
-    public function __invoke(ControllerEvent $event)
+    public function __invoke(RequestEvent $event)
     {
+        $controllerResult = $event->getControllerResult();
 
-        error_log('Should be implemented but it is not.');
-//        $controllerResult = $event->getControllerResult();
-//
-//        $callback = $this->app['callback_resolver']->resolveCallback($this->callback);
-//
-//        if (!$this->shouldRun($callback, $controllerResult)) {
-//            return;
-//        }
-//
-//        $response = call_user_func($callback, $controllerResult, $event->getRequest());
-//
-//        if ($response instanceof Response) {
-//            $event->setResponse($response);
-//        } elseif (null !== $response) {
-//            $event->setControllerResult($response);
-//        }
+        $callback = $this->app['callback_resolver']->resolveCallback($this->callback);
+
+        if (!$this->shouldRun($callback, $controllerResult)) {
+            return;
+        }
+
+        $response = call_user_func($callback, $controllerResult, $event->getRequest());
+
+        if ($response instanceof Response) {
+            $event->setResponse($response);
+        } elseif (null !== $response) {
+            $event->setControllerResult($response);
+        }
     }
 
     private function shouldRun($callback, $controllerResult)
